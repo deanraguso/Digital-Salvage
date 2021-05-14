dd10ae1518547b203b34a564c1e360b40a009259
dean <deanraguso@gmail.com>
2021-05-14 18:56:30 +1000

    :bug:Fixed: Added authentication to prevent other users from editing others addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,37 +1,13 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: %i[ show edit update destroy ]
-
-  # GET /addresses or /addresses.json
-  def index
-    @addresses = Address.all
-  end
+  before_action :authorized_user?
 
   # GET /addresses/1 or /addresses/1.json
   def show
   end
 
-  # GET /addresses/new
-  def new
-    @address = Address.new
-  end
-
   # GET /addresses/1/edit
   def edit
-  end
-
-  # POST /addresses or /addresses.json
-  def create
-    @address = Address.new(address_params)
-
-    respond_to do |format|
-      if @address.save
-        format.html { redirect_to @address, notice: "Address was successfully created." }
-        format.json { render :show, status: :created, location: @address }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @address.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /addresses/1 or /addresses/1.json
@@ -65,5 +41,17 @@ class AddressesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def address_params
       params.require(:address).permit(:country, :state, :postal_code, :street_address, :premise, :sub_premise)
+    end
+
+    # Only an authorized user may view or edit their own address
+    def authorized_user?
+      unless(user_signed_in?)
+        # A user who hasn't signed in, shouldn't be able to view or interact with addresses.
+        redirect_to root_path
+      end
+      # Ensure the current_user is only interacting with their own address.
+      unless(@address.id == current_user.address_id)
+        redirect_to root_path
+      end
     end
 end
