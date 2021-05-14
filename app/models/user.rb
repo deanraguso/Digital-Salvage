d7c92a955fcd4906eaa9c932566fa2994a37b33f
dean <deanraguso@gmail.com>
2021-05-14 17:33:42 +1000

    :sparkles:Created: Edit form, with prefil using existing data and the has_many, belongs to relationship between user and address.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :address
+  belongs_to :address
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
