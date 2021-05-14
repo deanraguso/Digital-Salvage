c37d8fa1f445fac257507b385ed63177bb79e852
dean <deanraguso@gmail.com>
2021-05-14 20:03:02 +1000

    :sparkles:Created: Seed file that makes 12 unique users and 6 unique addresses

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -10,6 +10,20 @@ class AddressesController < ApplicationController
   def edit
   end
 
+  def create
+    @address = Address.new(address_params)
+
+    respond_to do |format|
+      if @address.save
+        format.html { redirect_to @address, notice: "Address was successfully created." }
+        format.json { render :show, status: :created, location: @address }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @address.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   # PATCH/PUT /addresses/1 or /addresses/1.json
   def update
     respond_to do |format|
