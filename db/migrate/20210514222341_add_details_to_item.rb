f68514ca258d27fccf3e864335338bbb6075183f
dean <deanraguso@gmail.com>
2021-05-15 08:24:29 +1000

    :sparkles:Created: Item type and image field, required in this feature branch

---
+++ db/migrate/20210514222341_add_details_to_item.rb
@@ -0,0 +1,6 @@
+class AddDetailsToItem < ActiveRecord::Migration[6.1]
+  def change
+    add_column :items, :type, :string
+    add_column :items, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_004412) do
+ActiveRecord::Schema.define(version: 2021_05_14_222341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2021_05_14_004412) do
     t.integer "seller_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "type"
+    t.string "image"
   end
 
   create_table "orders", force: :cascade do |t|
