b5260617b04eb0aa2cfa93a5499e15f7199e9b3e
dean <deanraguso@gmail.com>
2021-05-14 14:30:51 +1000

    :sparkles:Created: Registration form fields for address and user names

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -10,9 +10,10 @@ class User::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -41,9 +42,12 @@ class User::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:country, :state, :postal_code,
+                                      :street_address, :premise, :sub_premise,
+                                      :email, :password, :password_encrypted,
+                                      :first_name, :last_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
