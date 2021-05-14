b6322e5403cd061e8bb351635d8acefaa8141129
dean <deanraguso@gmail.com>
2021-05-14 14:21:19 +1000

    :sparkles:Created: Devise controllers to override the new and create registration actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :addresses
   resources :items
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "user/registrations"
+  }
   root 'pages#index'
   get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
