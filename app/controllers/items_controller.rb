d96aab2465b8f4b701f3ace1aff7b057c7d5464e
dean <deanraguso@gmail.com>
2021-05-17 10:44:31 +1000

    :bug:Fixed: You can now create items without having to enter an id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
 
   # POST /items or /items.json
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(item_params.merge(user_id: current_user.id))
 
     respond_to do |format|
       if @item.save
