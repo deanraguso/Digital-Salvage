1e9b556f69a1fd865fce7e3a4d7dde8d30489655
dean <deanraguso@gmail.com>
2021-05-21 15:52:51 +1000

    :hammer:Created: User helper function, with common methods used in the user controllers.

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User::RegistrationsController < Devise::RegistrationsController
+  include UserHelper
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -11,25 +12,17 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    # Save address first, explicitly, so I can user it's id.
     address_params = params[:address].permit(:country, :state, :postal_code, :street_address, :premise, :sub_premise)
     address = Address.new(address_params)
-
-    # Save address first, explicitly, so I can user it's id.
-    unless address.save
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: address.errors, status: :unprocessable_entity }
-    end
+    address_saved = address.save
+    object_fail_save_message address_saved, address
 
     user_params = params[:user].permit(:first_name, :last_name, :email, :password, :password_confirmation).merge(:address_id => address.id)
     user = User.new(user_params)
-
-    if user.save!
-      sign_in(user)
-
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: user.errors, status: :unprocessable_entity }
-    end
+    user_saved = user.save
+    object_fail_save_message user_saved, user
+    user_saved || sign_in(user)
 
     redirect_to root_path
   end
@@ -41,12 +34,8 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
-    unless current_user.valid_password?(account_update_params[:current_password])
-      flash.alert = "Error: Password Incorrect!"
-      redirect_to edit_user_registration_path
-      return nil
-    end
-
+    correct_password_check account_update_params[:current_password]
+    
     pw_params = params[:user].permit(:current_password, :password, :password_confirmation)
     # Handle updating password if they added anything.
     if (current_user.valid_password?(pw_params[:password]) || current_user.valid_password?(pw_params[:password_confirmation]))
@@ -69,7 +58,7 @@ class User::RegistrationsController < Devise::RegistrationsController
     end
 
     flash.alert = "Success: Account has been updated!"
-    redirect_to root_path
+    redirect_to edit_user_registration_path
   end
 
   # DELETE /resource
---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,16 @@
+module UserHelper
+    def object_fail_save_message saved, object
+        unless saved
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: object.errors, status: :unprocessable_entity }
+        end
+    end
+
+    def correct_password_check pw
+        unless current_user.valid_password? pw
+            flash.alert = "Error: Password Incorrect!"
+            redirect_to edit_user_registration_path
+            return nil
+        end
+    end
+end
