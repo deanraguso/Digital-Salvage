1d68f37b344de1b8fefd481e2b9af0f02fee2922
dean <deanraguso@gmail.com>
2021-06-04 11:57:09 +1000

    Created: JS now formats the action cable messages like the default ones.

--- app/javascript/channels/inbox_channel.js
+++ app/javascript/channels/inbox_channel.js
@@ -2,6 +2,28 @@ import consumer from "./consumer"
 
 let url = window.location.href;
 
+function createMessage(id, content){
+  if (id == this_user_id){
+    return  `<div class="container-fluid row p-1 justify-content-end float-right" style="max-width: 900px">
+              <div class="container-fluid m-0 bg-primary text-white rounded">
+                  <strong>${this_user_email}: </strong>
+                  ${content}
+                  <p style="display:inline-block; width:200px; text-align:end" class="float-right">just now
+                </div>
+            </div>`
+  } else {
+    return   `<div class="container-fluid row p-1 justify-content-start" style="max-width: 900px"s>
+                <div class="container-fluid  m-0 bg-dark text-white rounded">
+                    <strong>${other_user_email}: </strong>
+                    ${content}
+                    <p style="display:inline-block; width:200px; text-align:end" class="float-right">just now
+                  </div>
+              </div>`
+  }
+
+  
+}
+
 if(url.indexOf("inboxes/") !== -1){
   let inbox_id = parseInt(url.substring(url.search("inboxes/") + 8));
 
@@ -17,8 +39,9 @@ if(url.indexOf("inboxes/") !== -1){
 
     received(data) {
       // Called when there's incoming data on the websocket for this channel
-      console.log(data);
-      document.querySelector("#messages").insertAdjacentHTML("beforeend", `<td>${data.text}</td>`)
+      console.log(data)
+      document.querySelector("#messages").insertAdjacentHTML("afterbegin", createMessage(data.user_id, data.content))
     }
   });
+  
 }
